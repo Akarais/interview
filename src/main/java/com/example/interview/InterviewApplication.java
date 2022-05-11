fa2e2701d3d05614f14eec3b4d68d7a05c415f02
razvan <razv@html24.net>
2022-05-11 16:06:12 +0300

    Exclude target from git, comment out spring boot data jpa until we make the mysql connection because it crashes otherwise

--- src/main/java/com/example/interview/InterviewApplication.java
+++ src/main/java/com/example/interview/InterviewApplication.java
@@ -2,12 +2,20 @@ package com.example.interview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class InterviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterviewApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world";
 	}
 
 }
